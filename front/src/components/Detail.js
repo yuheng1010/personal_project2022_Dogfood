08bd916ef2d219dc48092475b2f8c172d5f13e56
Heng <henhen90@gmail.com>
2022-12-16 03:41:11 +0800

    Second commit

--- front/src/components/Detail.js
+++ front/src/components/Detail.js
@@ -5,6 +5,7 @@ import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import Carousel from 'react-bootstrap/Carousel';
 import './Detail.css';
+import afoot from "../imgs/afoot.png"
 
 function Detail() {
     const { id } = useParams()
@@ -22,45 +23,77 @@ function Detail() {
             const img = JSON.parse(datas.img)
             if (Object.keys(img).length > 1) {
                 return (
-                    <Col xs={4} lg={4} md={4} xl={4} > 
-                    <Carousel variant="dark"style={{width:"350px",marginRight:"550px"}}>
-                        {Object.values(img).map((inner,index)=>
-                            <Carousel.Item >
-                            <img style={{width:"350px"}}
-                                className="img-fluid img-thumbnail rounded d-block w-100"
-                                src={inner}
-                            />
-                            </Carousel.Item>
-                        )}
-                    </Carousel>
+                    <Col xs={4} lg={4} md={4} xl={4} >
+                        <Carousel variant="dark" style={{ width: "350px", marginRight: "550px" }}>
+                            {Object.values(img).map((inner, index) =>
+                                <Carousel.Item >
+                                    <img style={{ width: "350px" }}
+                                        className="img-fluid  d-block w-100 pics"
+                                        src={inner}
+                                    />
+                                </Carousel.Item>
+                            )}
+                        </Carousel>
                     </Col>
                 )
             } else {
-                return (<img className="img-fluid img-thumbnail rounded dpic" src={Object.values(img)[0]}></img>)
+                return (<img className="img-fluid dpic" src={Object.values(img)[0]} ></img>)
             }
         }
     }
     function comparePrice() {
+        if (datas.price) {
+            const url = Object.keys(JSON.parse(datas.price))
+            const sortPri = []
+            const sortUrl = []
+            sortPri.push(Object.values(JSON.parse(datas.price)).sort(function (a, b) { return a - b; }))
+            for (var i = 0; i < sortPri[0].length; i++) {
+                sortUrl.push(url[Object.values(JSON.parse(datas.price)).indexOf(sortPri[0][i])])
+            }
+            return (
+                <div>
+                    {sortUrl.map((inner, index) => {
+                        if (inner.includes("pchome") === true) {
+                            return (<div><a className="brandUrl" href={inner}><img className="afoot" src={afoot}/>Pchome : </a><font style={{fontSize:"4px"}}> &ensp;TWD. </font><font>{sortPri[0][index]}</font></div>)
+                        }
+                        if (inner.includes("maoup") === true) {
+                            return (<div><a className="brandUrl" href={inner}><img className="afoot" src={afoot}/>Mao Up : </a><font style={{fontSize:"4px"}}> &ensp;TWD. </font><font>{sortPri[0][index]}</font></div>)
+                        }
+                        if (inner.includes("momo") === true) {
+                            return (<div><a className="brandUrl" href={inner}><img className="afoot" src={afoot}/>Momo : </a><font style={{fontSize:"4px"}}> &ensp;TWD.</font><font>{sortPri[0][index]}</font></div>)
+                        }
+                    }
 
+                    )}
+                </div>
+            )
+        }
     }
     return (
-        <div className='detailsAll'>
-            <Container >
-                <Row className='justify-content-center'>
-                    {/* <Col xs={4} lg={4} md={4} xl={4} > */}
+        <div >
+            <div className='detailsAll'>
+                <Container >
+                    <Row className='justify-content-center'>
                         {imgs()}
-                    {/* </Col> */}
-                    <Col xs={4} lg={4} md={4} xl={4} >
-                        <h5>{datas.name}</h5>
-                        <br />
-                        <font>編號 : {datas.id}</font><br />
-                        <font>廠商 : {datas.brand}</font>
-                        <hr />
-                        <br /><br />
-                    </Col>
-                </Row >
-                <br />
-            </Container>
+                        <div className='detail' >
+                            <h5>{datas.name}</h5>
+                            <font className="pID">Product ID :</font><font> {datas.id}</font><br />
+                            <font className="pBrand">Brand : {datas.brand}</font>
+                            <br /><br />
+                            <div className='compareArea'>
+                                {comparePrice()}
+                            </div>
+                            <div>
+                            <button className="addCommentBtn" id="addCommentBtn" >新增評論</button>
+                            <button className="showCommentBtn" id="showCommentBtn" >查看評價</button>
+                            </div>
+                        </div>
+                    </Row >
+                    <br />
+                </Container>
+            </div>
+            {/* <div className="commentArea">
+            </div> */}
         </div>
     )
 }
--- front/src/components/Login.js
+++ front/src/components/Login.js
@@ -1,41 +1,75 @@
-import Container from 'react-bootstrap/Container';
-import Form from "react-bootstrap/Form";
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Button from 'react-bootstrap/Button';
 import React, { useEffect, useState } from 'react'
 import './Login.css'
 
 
 function Login() {
     useEffect(() => {
-        const container = document.querySelector('.container')
-        container.addEventListener('animationend', () => {
-            container.classList.remove('active');
+        console.clear();
+
+        const loginBtn = document.getElementById('login');
+        const signupBtn = document.getElementById('signup');
+
+        loginBtn.addEventListener('click', (e) => {
+            let parent = e.target.parentNode.parentNode;
+            Array.from(e.target.parentNode.parentNode.classList).find((element) => {
+                if (element !== "slide-up") {
+                    parent.classList.add('slide-up')
+                } else {
+                    signupBtn.parentNode.classList.add('slide-up')
+                    parent.classList.remove('slide-up')
+                }
+            });
+        });
+
+        signupBtn.addEventListener('click', (e) => {
+            let parent = e.target.parentNode;
+            Array.from(e.target.parentNode.classList).find((element) => {
+                if (element !== "slide-up") {
+                    parent.classList.add('slide-up')
+                } else {
+                    loginBtn.parentNode.parentNode.classList.add('slide-up')
+                    parent.classList.remove('slide-up')
+                }
+            });
         });
     }, [])
     return (
-        <Container>
-            <section id='loginSection'>
-                <br /><br />
-                <Row><font size='7'>Login</font></Row>
-                <Row><hr style={{ color: 'black' }} /></Row>
-                <br /><br />
-                <label for="uAc">帳號:</label>
-                <br />
-                <Form.Control type='text' style={{ borderRadius: "7px" }} className="uAc" name='uAc' id="uAc" />
-                <br />
-                <label for="uPa">密碼:</label>
-                <br />
-                <Form.Control type='password' style={{ borderRadius: "7px" }} className="uPa" name='uPa' id="uPa" />
-                <br /><br />
-                <Row className='justify-content-center'><Col xs={3} lg={2} md={2} xl={1}><Button id="signI" style={{ backgroundColor: '#AFDFE4', borderRadius: "10px", color: 'green' }}>登入</Button></Col>
-                    <Col xs={3} lg={2} md={2} xl={1}><Button id="signU" style={{ backgroundColor: '#AFDFE4', borderRadius: "10px", color: 'green' }}>註冊</Button></Col></Row>
 
-            </section>
-            <Row className='justify-content-center' id="ajax"></Row>
-            
-        </Container>
+        <div className='LoginSection'>
+            <div className="form-structor">
+                <div className="signup">
+                    <h2 className="form-title" id="signup"><span>or</span>Log in</h2>
+                    <div className="form-holder">
+
+                        <input type="email" className="input" placeholder="Email" />
+                        <input type="password" className="input" placeholder="Password" />
+                    </div>
+                    <button className="submit-btn">Log in</button>
+                </div>
+                <div className="login slide-up">
+                    <div className="center">
+                        <h2 className="form-title" id="login"><span>or</span>Sign up</h2>
+                        <div className="form-holder">
+                            <input type="text" className="input" placeholder="User's name" />
+                            <input type="email" className="input" placeholder="Email" />
+                            <input type="password" className="input" placeholder="Password" />
+                            <input type="text" className="input" placeholder="phone" />
+                            <input type="text" className="input" placeholder="Dog's name" />
+                            <input type="text" className="input" placeholder="Dog's age" />
+                            <select className='dogVar' placeholder="Dog's size">
+                                <option select hidden>Dog's size</option>
+                                <option>Large-sized</option>
+                                <option>Medium-sized</option>
+                                <option>Small-sized</option>
+                            </select>
+                        </div>
+
+
+                        <button className="submit-btn">Sign Up</button>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 export default Login
